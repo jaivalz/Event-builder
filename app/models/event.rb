5d6dbf43036ce11b41e81ef003b4204f2e89c73d
Jaime <jaivalz@yahoo.com>
2016-04-14 00:32:53 +0200

    Added tag based search

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,19 @@ class Event < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).events
+  end
+
+  def self.tag_counts
+    Tag.select("tags.name, count(taggings.tag_id) as count").
+    joins(:taggings).group("taggings.tag_id")
+  end
+
   def all_tags
     tags.map(&:name).join(", ")
   end
-  
+
   def all_tags=(names)
     self.tags = names.split(",").map do |n|
       Tag.where(name: n.strip).first_or_create!
