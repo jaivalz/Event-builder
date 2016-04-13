d4b31279fdd9f56a318b422884f2a2c47d17e73c
Jaime <jaivalz@yahoo.com>
2016-04-13 22:51:17 +0200

    Added custom permissions for users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :event_owner!, only: [:show, :edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
@@ -63,6 +64,15 @@ class EventsController < ApplicationController
   end
 
   private
+
+    def event_owner!
+      authenticate_user!
+      if @event.organizer_id != current_user.id
+        redirect_to events_path
+        flash[:notice] = "Not permitted"
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
